170e57739a42c8449ab4d141473adf5b9a1058f3
SofiiaChurikova <sonyachurikova@gmail.com>
2024-05-31 22:44:45 +0300

    Create func which makes img from array
    add func for two arrays to show transformations but on images

--- second_part.py
+++ second_part.py
@@ -47,34 +47,93 @@ def universal_transformation(image, matrix):
     return transformed_image
 
 
+def create_image_from_figure(figure, size=(900, 900), color='r'):
+    fig, ax = plt.subplots()
+    ax.plot(figure[:, 0], figure[:, 1], color)
+    ax.set_xlim([-1.5, 1.5])
+    ax.set_ylim([-1.5, 1.5])
+    ax.axis('off')
+    fig.canvas.draw()
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    plt.close(fig)
+    image = cv2.resize(image, size, interpolation=cv2.INTER_LINEAR)
+    return image
+
+
 image = cv2.imread("31aca9782309cc934ce0066c2c81ab2f.jpeg")
+
+batman_figure = np.array([
+    [0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4],
+    [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
+flower_figure = np.array([
+    [0, 0], [0.2, 0.5], [0.5, 0.2], [0.7, 0.7], [1, 0.4],
+    [0.5, 1], [0, 0.8], [-0.5, 1], [-1, 0.4], [-0.7, 0.7],
+    [-0.5, 0.2], [-0.2, 0.5], [0, 0]
+])
+
+batman_image = create_image_from_figure(batman_figure)
+flower_image = create_image_from_figure(flower_figure)
+
 # ------------------------------------
 # Фото без перетворень
 
 # plot_image(image)
+# plot_image(batman_image)
+# plot_image(flower_image)
 # ------------------------------------
 # Фото обернене на певний кут
 
 # rotated_image = rotation(image, 45)
 # plot_image(rotated_image)
+#
+# rotated_batman = rotation(batman_image, 45)
+# plot_image(rotated_batman)
+#
+# rotated_flower = rotation(flower_image, 60)
+# plot_image(rotated_flower)
 # ------------------------------------
 # Фото маштабоване з певним коефіцієнтом
 
 # scaled_image = scale(image, 2)
 # plot_image(scaled_image)
+
+# scaled_batman = scale(batman_image, 2)
+# plot_image(scaled_batman)
+#
+# scaled_flower = scale(flower_image, 4)
+# plot_image(scaled_flower)
 # ------------------------------------
 # Фото відзеркалене за певної осі
 
 # reflected_image = axis_reflection(image, "x")
 # plot_image(reflected_image)
+
+reflected_batman = axis_reflection(batman_image, "x")
+plot_image(reflected_batman)
+#
+# reflected_flower = axis_reflection(flower_image, "y")
+# plot_image(reflected_flower)
 # ------------------------------------
 # Нахил певної осі координат
 
 # shear_image = shear_axis(image, 20, "x")
 # plot_image(shear_image)
+
+# shear_batman = shear_axis(batman_image, 35, "x")
+# plot_image(shear_batman)
+#
+# shear_flower = shear_axis(flower_image, 20, "y")
+# plot_image(shear_flower)
 # ------------------------------------
 # Трансформація з переданою у функцію кастомною матрицею трансформації
 
 # transformed_image = universal_transformation(image, np.float32([[0, 1, 1], [1, 1, 0]]))
 # plot_image(transformed_image)
+
+# transformed_batman = universal_transformation(batman_image, np.float32([[1, 0, 1], [1, 0.5, 0]]))
+# plot_image(transformed_batman)
+#
+# transformed_flower = universal_transformation(flower_image, np.float32([[0.5, 0, 0], [1, 0.5, 0]]))
+# plot_image(transformed_flower)
 # ------------------------------------
